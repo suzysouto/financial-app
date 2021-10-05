3d8d1f2482b06aee66cd7cefc91bd8ea508d22b0
suzysouto <suzysoutosilva@gmail.com>
2021-10-05 20:00:18 -0300

    add ghost button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,15 +1,35 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import tw from "tailwind-styled-components";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
+    variant?: "ghost";
 }
 
-export const Button = ({ children, ...props }: Props) => {
-    return (
-        <button {...props}
-        className="bg-purple-500 h-10 text-white font-bold 
-        hover:bg-purple-400 disabled:bg-gray-200 disabled:cursor-not-allowed">{children}
-        </button>
+export const Button = ({ children, variant, ...props }: Props) => {
+    if (variant === 'ghost') {
+        return (
+            <GhostButton {...props}
+                className="">{children}
+            </GhostButton>
+        );
+    };
 
+    return (
+        <MainButton {...props}
+        className="">{children}
+        </MainButton>
     );
 };
+
+export const BaseButton = tw.button`
+    h-10 px-8 text-white font-bold disabled:bg-gray-200 disabled:cursor-not-allowed
+`;
+
+export const MainButton = tw(BaseButton)`
+    bg-purple-500 hover:bg-purple-400
+`;
+
+export const GhostButton = tw(BaseButton)`
+    bg-transparent text-purple-500 hover:bg-purple-100
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,17 @@
+import { Button } from "../../components/atoms/Button";
+import { TemplateContainer } from "../../components/templates";
+
 export const Dashboard = () => {
-    return <div>Dashboard Controlled</div>
+    return ( 
+        <TemplateContainer>
+            <header className="w-full max-w-screen-lg flex justify-between items-center h-1/6">
+                <span className="font-bold text-2xl">Expanse Tracker</span>
+
+                <nav className="flex gap-4">
+                    <Button>Adicionar Nova Conta</Button>
+                    <Button variant="ghost">Sair</Button>
+                </nav>
+            </header>
+        </TemplateContainer>
+    );
 };
